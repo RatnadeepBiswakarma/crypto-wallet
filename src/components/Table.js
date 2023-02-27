94d4b77e1c35a614156627c55778bc7ca98399dd
Ratnadeep <ratnadeeppersonal@gmail.com>
2023-02-27 18:37:15 +0530

    feat: modified table component to pass header and body

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,40 +4,46 @@ function formatPrice(price) {
   return price.toFixed(3)
 }
 
-export default function Table({ items, handleUnitsInput }) {
+export default function Table({ items, head, body, handleUnitsInput }) {
   return (
     <table>
       <thead>
-        <tr className='border-b border-gray-700'>
-          <th className='px-2 pb-2'>Name</th>
-          <th className='px-2 pb-2'>Symbol</th>
-          <th className='px-2 pb-2'>Price</th>
-          <th className='px-2 pb-2'>Purchase Units</th>
-        </tr>
+        {head ? (
+          head
+        ) : (
+          <tr className='border-b border-gray-700'>
+            <th className='px-2 pb-2'>Name</th>
+            <th className='px-2 pb-2'>Symbol</th>
+            <th className='px-2 pb-2'>Price</th>
+            <th className='px-2 pb-2'>Purchase Units</th>
+          </tr>
+        )}
       </thead>
 
       <tbody>
-        {items.map(item => {
-          return (
-            <tr key={item.id}>
-              <td className='px-2'>{item.slug}</td>
-              <td className='px-2'>{item.symbol}</td>
-              <td className='px-2 text-green-500'>
-                $ {formatPrice(item.metrics.market_data.price_usd)}
-              </td>
-              <td className='px-2 units'>
-                <input
-                  type='number'
-                  step='0.01'
-                  value={item.units}
-                  onChange={evt => handleUnitsInput(item, evt.target.value)}
-                  placeholder='Units to purchase'
-                  className='bg-transparent w-full px-2 border-b border-gray-700'
-                />
-              </td>
-            </tr>
-          )
-        })}
+        {body
+          ? body
+          : items.map(item => {
+              return (
+                <tr key={item.id}>
+                  <td className='px-2'>{item.slug}</td>
+                  <td className='px-2'>{item.symbol}</td>
+                  <td className='px-2 text-green-500'>
+                    $ {formatPrice(item.metrics.market_data.price_usd)}
+                  </td>
+                  <td className='px-2 units'>
+                    <input
+                      type='number'
+                      step='0.01'
+                      value={item.units}
+                      onChange={evt => handleUnitsInput(item, evt.target.value)}
+                      placeholder='Units to purchase'
+                      className='bg-transparent w-full px-2 border-b border-gray-700'
+                    />
+                  </td>
+                </tr>
+              )
+            })}
       </tbody>
     </table>
   )
